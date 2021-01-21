1322fd9ae6b463e3640195d8749c810a0f2a4b4d
kevin.juguin <kevin.juguin@epsi.fr>
2021-01-21 23:11:19 +0100

    Add CSS for brand, drug, vaccine, people and injection

--- ehpad.WEB/wwwroot/js/site.js
+++ ehpad.WEB/wwwroot/js/site.js
@@ -2,3 +2,54 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+console.log(window.location.pathname);
+console.log(window.location.pathname.split('/'));
+console.log(window.location.pathname.split('/')[0]);
+console.log(window.location.pathname.split('/')[1]);
+
+
+$(document).ready(function () {
+
+    console.log(window.location.pathname);
+    var path = window.location.pathname.split('/')[1];
+    console.log(path);
+    switch (path) {
+        case "Drugs":
+            $('.drugs')
+                .addClass('active');
+            break;
+        case "Brands":
+            $('.brands')
+                .addClass('active');
+            break;
+        case "Vaccines":
+            $('.vaccines')
+                .addClass('active');
+            break;
+        case "Injections":
+            $('.injections')
+                .addClass('active');
+            break;
+        case "Drugs":
+            $('.drugs')
+                .addClass('active');
+            break;
+        case "People":
+            $('.peoples')
+                .addClass('active');
+            break;
+        default :
+            $('.filters')
+                .addClass('active');
+
+    }
+
+    $('ul.navbar-nav > li')
+        .click(function (e) {
+            $('ul.navbar-nav > li')
+                .removeClass('active');
+            $(this).addClass('active');
+        });
+});
