69abf832b9df378e0e76d6b2e0e6ffe03edb2402
FlorentRenou <florent.renou@epsi.fr>
2021-01-31 11:47:02 +0100

    Réalisation du filtre dans une version plus fonctionnelle.

--- ehpad.WEB/Controllers/FilterController.cs
+++ ehpad.WEB/Controllers/FilterController.cs
@@ -36,34 +36,79 @@ namespace ehpad.WEB.Controllers
         // GET: Filter/IndexFiltre3
         //   [HttpPost]
         //   [ValidateAntiForgeryToken]
-        public async Task<IActionResult> IndexFiltre3()
+        public ActionResult IndexFiltre3()
         {
-            DateTime date = DateTime.Today;
-            return View(await _context.Drugs.ToListAsync());
+            IEnumerable<SelectListItem> selectList = from d in _context.Drugs
+                                                     select new SelectListItem
+                                                     {
+                                                         Value = d.Id.ToString(),
+                                                         Text = d.Name
+                                                     };
+            ViewData["Drug"] = new SelectList(
+               selectList,
+                "Value",
+                "Text");
+            ViewData["Injection"] = null;
+            return View();
         }
 
         // GET: Filter/Filtre3
         // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        public async Task<IActionResult> Filtre3(int? id, int? date)
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598
+        public async Task<IActionResult> Filtre3([Bind("Id")] Drug drug, bool date)
         {
+            IEnumerable<SelectListItem> selectList = from d in _context.Drugs
+                                                     select new SelectListItem
+                                                     {
+                                                         Value = d.Id.ToString(),
+                                                         Text = d.Name
+                                                     };
+            ViewData["Drug"] = new SelectList(
+               selectList,
+                "Value",
+                "Text");
 
-            if(date is null) {
-                return View(await _context.Injections
+
+            List<People> personneNonVaccineeListe = new List<People>();
+            List<People> personneListe = await _context.Peoples.ToListAsync();
+            List<Injection> personneVaccineeListe = new List<Injection>();
+
+            if (date is false)
+            {
+                personneVaccineeListe = await _context.Injections
                    .Include("People")
-                   .Where(m => m.VaccineId == id)
-                   .ToListAsync());
+                   .Where(m => m.VaccineId == drug.Id)
+                   .ToListAsync();
             }
             else
             {
                 DateTime AnneeActuelle = new DateTime(DateTime.Today.Year, 1, 1);
-                return View(await _context.Injections
+                personneVaccineeListe = await _context.Injections
                    .Include("People")
-                   .Where(m => m.VaccineId == id)
-                   .Where(m => m.VaccineDate > AnneeActuelle)
-                   .ToListAsync());
+                   .Where(m => m.VaccineDate >= AnneeActuelle)
+                   .Where(m => m.VaccineId == drug.Id)
+                   .ToListAsync();
             }
-                
+            
+            personneListe.ForEach(delegate (People people)
+            {
+                bool present = false;
+                personneVaccineeListe.ForEach(delegate (Injection injection)
+                {
+                    if (injection.PeopleId == people.Id)
+                    {
+                        present = true;
+                    }
+                });
+                if (present is false)
+                {
+                    personneNonVaccineeListe.Add(people);
+                }
+            });     
+
+            ViewData["People"] = personneNonVaccineeListe;
+
+            return View("IndexFiltre3");
         }
 
         // POST: Filter/Filtre2
